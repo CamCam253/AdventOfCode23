e5031b2730f4dddcb08fd28bb928a29ea0b9b369
CamCam253 <camiselkala@gmail.com>
2023-12-02 23:37:46 +0200

    Working part 2

--- Day2/ColorGameAnalysis/ColorGameAnalysis/Program.cs
+++ Day2/ColorGameAnalysis/ColorGameAnalysis/Program.cs
@@ -1,8 +1,8 @@
 ﻿// See https://adventofcode.com/2023/day/2 for the assignment
 string fileName = "..\\..\\..\\input.txt";
-const int maxRedCubes = 12;
-const int maxGreenCubes = 13;
-const int maxBlueCubes = 14;
+//const int maxRedCubes = 12;
+//const int maxGreenCubes = 13;
+//const int maxBlueCubes = 14;
 int sum = 0;
 
 using (StreamReader reader = new StreamReader(fileName))
@@ -11,10 +11,13 @@ using (StreamReader reader = new StreamReader(fileName))
     while ((line = reader.ReadLine()) != null)
     {
         //a game is believed to be true unless proven false
-        bool possibleGame = true;
+        //bool possibleGame = true;
         string[] splitLine = line.Split(':');
         string[] gameSets = splitLine[1].Split(";");
-        int gameNum = int.Parse(splitLine[0].Split(" ")[1]);
+        //int gameNum = int.Parse(splitLine[0].Split(" ")[1]);
+        int minRed = 0;
+        int minGreen = 0;
+        int minBlue = 0;
 
         foreach (string cubeSet in gameSets)
         {
@@ -26,40 +29,30 @@ using (StreamReader reader = new StreamReader(fileName))
                 switch (pair[1])
                 {
                     case "red":
-                        if (cubeNum > maxRedCubes)
+                        if (cubeNum > minRed)
                         {
-                            possibleGame = false;
+                            minRed = cubeNum;
                         }
                         break;
                     case "green":
-                        if (cubeNum > maxGreenCubes)
+                        if (cubeNum > minGreen)
                         {
-                            possibleGame = false;
+                            minGreen = cubeNum;
                         }
                         break;
                     case "blue":
-                        if (cubeNum > maxBlueCubes)
-                        { 
-                            possibleGame = false; 
+                        if(cubeNum > minBlue)
+                        {
+                            minBlue = cubeNum;
                         }
                         break;
                     default: 
                         break;
                 }
-                if (!possibleGame)
-                {
-                    break;
-                }
-            }
-            if (!possibleGame)
-            {
-                break;
             }
         }
-        if (possibleGame)
-        {
-            sum += gameNum;
-        }
+        int power = minRed * minGreen * minBlue;
+        sum += power;
     }
 }
 
