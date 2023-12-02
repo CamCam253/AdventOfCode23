52cf65078b504907674d7e71c56a5f8ca29f8da4
CamCam253 <camiselkala@gmail.com>
2023-12-02 23:12:38 +0200

    cases for colors and loops

--- Day2/ColorGameAnalysis/ColorGameAnalysis/Program.cs
+++ Day2/ColorGameAnalysis/ColorGameAnalysis/Program.cs
@@ -1,8 +1,38 @@
 ﻿// See https://adventofcode.com/2023/day/2 for the assignment
+string fileName = "..\\..\\..\\input.txt";
+const int maxRedCubes = 12;
+const int maxGreenCubes = 13;
+const int maxBlueCubes = 14;
 
-string fileName = Path.GetFileName("input.txt");
-
-using (StreamReader reader = new StreamReader(fileName)
+using (StreamReader reader = new StreamReader(fileName))
 {
+    string line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        //a game is believed to be true unless proven false
+        bool possibleGame = true;
+        string[] splitLine = line.Split(':');
+        string[] cubeSets = splitLine[1].Split(";");
+        int gameNum = int.Parse(splitLine[0].Split(" ")[1]);
 
+        foreach (string cubeSet in cubeSets)
+        {
+            string[] cubes = cubeSet.Trim().Split(",");
+            foreach (string cube in cubes)
+            {
+                string[] pair = cube.Split(" ");
+                switch (pair[1])
+                {
+                    case "red":
+                        break;
+                    case "green":
+                        break;
+                    case "blue":
+                        break;
+                    default: 
+                        break;
+                }
+            }
+        }
+    }
 }
