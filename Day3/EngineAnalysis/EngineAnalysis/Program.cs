78846ae6dc4fe37734ab3f3045fce32464e3e770
CamCam253 <camiselkala@gmail.com>
2023-12-03 18:40:07 +0200

    Inital Commit Day 3
    program reads input and saves it with in the program

---
+++ Day3/EngineAnalysis/EngineAnalysis/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://adventofcode.com/2023/day/3 for the assignment
+string fileName = "..\\..\\..\\input.txt";
+string[,] data = {};
+
+using (StreamReader reader = new StreamReader(fileName))
+{
+    string line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        int row = 0;
+        for (int col = 0; col < line.Length; col++)
+        {
+            data[row, col] = line.Substring(col, 1).Trim();
+        }
+    }
+}
