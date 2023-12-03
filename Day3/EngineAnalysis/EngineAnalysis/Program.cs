6ab37fe5f96ab8e4a5e32010cb40b015d0b68280
CamCam253 <camiselkala@gmail.com>
2023-12-03 19:05:53 +0200

    Program can check if place is symb

--- Day3/EngineAnalysis/EngineAnalysis/Program.cs
+++ Day3/EngineAnalysis/EngineAnalysis/Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://adventofcode.com/2023/day/3 for the assignment
 string fileName = "..\\..\\..\\input.txt";
 string[,] data = {};
+int sum1;
+
 
 using (StreamReader reader = new StreamReader(fileName))
 {
@@ -10,7 +12,36 @@ using (StreamReader reader = new StreamReader(fileName))
         int row = 0;
         for (int col = 0; col < line.Length; col++)
         {
-            data[row, col] = line.Substring(col, 1).Trim();
+            string sub = line.Substring(col, 1).Trim();
+            if (sub.Equals("."))
+            {
+                data[row, col] = " ";
+            }
+            else
+            {
+                 data[row, col] = sub;
+            }
+        }
+        row++;
+    }
+}
+
+for (int row = 0; row < data.GetLength(0); row++)
+{
+    for (int col = 0; col < data.GetLength(1); col++)
+    {
+        string temp = data[row, col];
+        if (!temp.Equals(" "))
+        {
+            if (!Char.IsDigit(temp, 0))
+            {
+                GetNumbers();
+            }
         }
     }
+}
+
+static int[] GetNumbers()
+{
+    return null;
 }
